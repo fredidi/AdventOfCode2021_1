7dcb42f54ea6c2fe12d197ec75b12b8820ae8770
Fredrik Lam <fredrik.didi.lam@gmail.com>
2021-12-06 23:28:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string[] textFile = File.ReadAllLines(@"C:\Users\fredd\OneDrive\.NET Developer\AdventOfCode\1\input.txt");
+
+            //foreach (var text in textFile)
+            //{
+            //    Console.WriteLine(text);
+            //}
+
+            int[] list = new int[textFile.Length];
+            for (int i = 0; i < textFile.Length; i++)
+            {
+                list[i] = Convert.ToInt32(textFile[i]);
+            }
+            for (int i = 0; i < list.Length -1; i++)
+            {
+                if (list[i+1] > list[i])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
