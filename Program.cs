332bdea668e2e494a71a1d9e47cabc0ac820c35d
Fredrik Lam <fredrik.didi.lam@gmail.com>
2021-12-14 22:39:31 +0100

    Update PartTwo

--- Program.cs
+++ Program.cs
@@ -6,42 +6,41 @@ namespace _1
 {
     class Program
     {
-
-        static void Main(string[] args)
+        public static void PartOne()
         {
             int count = 0;
             string[] textFile = File.ReadAllLines(@"C:\Users\fredd\OneDrive\.NET Developer\AdventOfCode\1\input.txt");
 
-            //foreach (var text in textFile)
-            //{
-            //    Console.WriteLine(text);
-            //}
-
-            //PART ONE
             int[] list = new int[textFile.Length];
             for (int i = 0; i < textFile.Length; i++)
             {
                 list[i] = Convert.ToInt32(textFile[i]);
             }
-            //for (int i = 0; i < list.Length -1; i++)
-            //{
-            //    if (list[i+1] > list[i])
-            //    {
-            //        count++;
-            //    }
-            //}
-            //Console.WriteLine(count);
-
-
-
-
+            for (int i = 0; i < list.Length - 1; i++)
+            {
+                if (list[i + 1] > list[i])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Part 1 solution:\n" + count); //1482
+        }
+        public static void PartTwo()
+        {
             //PART TWO
 
             //a = i
             //b = i+1
             //c = i+2
             //sum = (a+b+c)
+            int count = 0;
+            string[] textFile = File.ReadAllLines(@"C:\Users\fredd\OneDrive\.NET Developer\AdventOfCode\1\input.txt");
 
+            int[] list = new int[textFile.Length];
+            for (int i = 0; i < textFile.Length; i++)
+            {
+                list[i] = Convert.ToInt32(textFile[i]);
+            }
             for (int i = 0; i < list.Length - 3; i++)
             {
                 int sum1 = (list[i]) + (list[i + 1]) + (list[i + 2]);
@@ -52,7 +51,15 @@ namespace _1
                     count++;
                 }
             }
-            Console.WriteLine(count);
+            Console.WriteLine("Part 2 solution:\n" + count);
+        }
+        static void Main(string[] args)
+        {
+            PartOne();
+            Console.WriteLine();
+            PartTwo();
+
+
         }
     }
 }
